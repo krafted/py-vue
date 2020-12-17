fed65fadf058299013cb04d018aadd9b95986bba
Sam <s@sammagee.me>
2020-12-17 11:03:05 -0600

    fix header padding

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,43 @@
 <template>
   <div class="flex flex-col h-screen">
-    <header class="flex items-center justify-between px-4 h-14 pt-safe-top">
-      <h1>
-        <a
-          class="flex items-center justify-center w-10 h-10 -ml-2 font-mono text-lg font-semibold text-gray-700 border border-transparent rounded-md select-none hover:bg-black focus:bg-black focus:border-gray-800 hover:border-gray-800 hover:text-gray-400 focus:text-gray-400 focus:outline-none"
-          href="/"
-        >
-          Py
-        </a>
-      </h1>
+    <header class="px-4 pt-safe-top">
+      <div class="flex items-center justify-between py-1">
+        <h1>
+          <a
+            class="flex items-center justify-center w-10 h-10 -ml-2 font-mono text-lg font-semibold text-gray-700 border border-transparent rounded-md select-none hover:bg-black focus:bg-black focus:border-gray-800 hover:border-gray-800 hover:text-gray-400 focus:text-gray-400 focus:outline-none"
+            href="/"
+          >
+            Py
+          </a>
+        </h1>
 
-      <div class="flex items-center justify-end -mr-2 space-x-2">
-        <button
-          class="flex items-center justify-center p-2.5 text-gray-700 border border-transparent rounded-md group hover:w-auto hover:bg-black focus:bg-black focus:border-gray-800 hover:border-gray-800 hover:text-gray-400 focus:text-gray-400 focus:outline-none focus:w-auto"
-          @click="showSettings = true"
-        >
-          <span class="sr-only">Settings</span>
+        <div class="flex items-center justify-end -mr-2 space-x-2">
+          <button
+            class="flex items-center justify-center p-2.5 text-gray-700 border border-transparent rounded-md group hover:w-auto hover:bg-black focus:bg-black focus:border-gray-800 hover:border-gray-800 hover:text-gray-400 focus:text-gray-400 focus:outline-none focus:w-auto"
+            @click="showSettings = true"
+          >
+            <span class="sr-only">Settings</span>
 
-          <span
-            v-if="!isMobile()"
-            class="hidden mr-2 font-mono text-xs group-hover:inline group-focus:inline"
-            v-text="isMac ? 'CMD + ,' : 'CTRL + ,'"
+            <span
+              v-if="!isMobile()"
+              class="hidden mr-2 font-mono text-xs group-hover:inline group-focus:inline"
+              v-text="isMac ? 'CMD + ,' : 'CTRL + ,'"
+            />
+
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+          </button>
+
+          <Settings
+            :is-mobile="isMobile()"
+            :settings="settings"
+            :show="showSettings"
+            @changed="setSetting($event.key, $event.value)"
+            @closed="showSettings = false"
           />
-
-          <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg>
-        </button>
-
-        <Settings
-          :is-mobile="isMobile()"
-          :settings="settings"
-          :show="showSettings"
-          @changed="setSetting($event.key, $event.value)"
-          @closed="showSettings = false"
-        />
+        </div>
       </div>
     </header>
 
